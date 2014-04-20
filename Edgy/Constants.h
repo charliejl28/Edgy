f088117a58cd436ee5b5457db21e1e2a15cad0e6
charliejl28 <cmj3@princeton.edu>
2014-04-19 21:17:26 -0400

    constants file

---
+++ Edgy/Constants.h
@@ -0,0 +1,28 @@
+//
+//  Constants.h
+//  Edgy
+//
+//  Created by Charlie Jacobson on 4/19/14.
+//  Copyright (c) 2014 Edgy. All rights reserved.
+//
+
+#ifndef Edgy_Constants_h
+#define Edgy_Constants_h
+
+//////// Colors //////////
+# pragma mark - Colors
+#define edgyBlue [UIColor colorWithRed:0.31 green:0.502 blue:0.741 alpha:1.0]
+
+/////// Fonts //////////
+# pragma mark - Fonts
+#define APPFONT @"Avenir"
+
+
+//Button Font Size
+#define BUTTONFONTSIZE 20
+
+
+
+
+
+#endif
